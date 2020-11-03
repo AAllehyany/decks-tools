335dbc4887067eaf456d77cc9ad21694c8bebd7c
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-03 16:52:25 +0300

    new stuff

--- inserter.js
+++ inserter.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 const fs = require('fs');
 
-
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYm90IiwidXNlcklkIjoxMCwiaWF0IjoxNjA0MjMyNDMxfQ.6VA0-lGDaiWvstkUTFnmN0RUMwFml8crPlp83rMLQEc";
 dotenv.config();
 const axiosConfig = {
     headers: {
-        'Authorization': `Bearer ${process.env.JWT_TOKEN}`,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
     }
 };
@@ -15,7 +15,7 @@ async function script(config) {
     try {
         const madokaData = await fs.readFileSync('./madoka.json');
         const data = JSON.parse(madokaData);
-        const resp = await axios.post('http://localhost:3030/weiss-cards', data, config);
+        const resp = await axios.post('http://localhost:3000/weiss-cards/create', data, config);
         if(resp.status != 200) {
             return console.log('error???');
         }
